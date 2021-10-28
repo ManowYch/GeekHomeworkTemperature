2f79348913ad93004f823c5b343447bc109d6bc5
ManowYch <ychepaykin@gmail.com>
2021-10-28 20:30:38 +0200

    Добавьте файлы проекта.

---
+++ GeekHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную температуру");
+            float tempMin = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру");
+            float tempMax = float.Parse(Console.ReadLine());
+            float tempAv = (tempMax + tempMin) / 2;
+            Console.WriteLine($"Среднесуточная температура составляет {tempAv}");
+            Console.ReadLine();
+        }
+    }
+}
